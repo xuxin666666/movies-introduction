ee4f1987484c07010f4cad53f8e3e7c40124e450
xuxin666666 <58601208+xuxin666666@users.noreply.github.com>
2021-09-05 14:14:28 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import App from './App.jsx';
+
+// axios.defaults.baseURL = 'http://47.106.218.69:8000/'
+axios.defaults.baseURL = 'http://localhost:8000/'
 
 ReactDOM.render(
     <App></App>,
